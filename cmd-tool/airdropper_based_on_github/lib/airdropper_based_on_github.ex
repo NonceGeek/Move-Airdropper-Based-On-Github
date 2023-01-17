77461e3ef386aa7e305fbfe7f955844c796dd61f
leeduckgo <albertschr@163.com>
2023-01-17 17:41:27 +0800

    init tools

---
+++ cmd-tool/airdropper_based_on_github/lib/airdropper_based_on_github.ex
@@ -0,0 +1,18 @@
+defmodule AirdropperBasedOnGithub do
+  @moduledoc """
+  Documentation for `AirdropperBasedOnGithub`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AirdropperBasedOnGithub.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ cmd-tool/airdropper_based_on_github/test/airdropper_based_on_github_test.exs
@@ -0,0 +1,8 @@
+defmodule AirdropperBasedOnGithubTest do
+  use ExUnit.Case
+  doctest AirdropperBasedOnGithub
+
+  test "greets the world" do
+    assert AirdropperBasedOnGithub.hello() == :world
+  end
+end
