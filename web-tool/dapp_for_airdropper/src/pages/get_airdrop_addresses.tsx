326a4e5fc1345e25a67f1d6d4a3c84f33eb6fc1c
Blockchain-Key <1416893908@qq.com>
2023-05-03 20:07:44 +0800

    #23

--- web-tool/dapp_for_airdropper/src/pages/get_airdrop_addresses.tsx
+++ web-tool/dapp_for_airdropper/src/pages/get_airdrop_addresses.tsx
@@ -1,48 +1,42 @@
-import { DAPP_ADDRESS, MODULE_NAME, MODULE_URL } from '../config/constants';
-import { useWallet } from '@manahippo/aptos-wallet-adapter';
-import { useState } from 'react';
-import React from 'react';
-import { get_aptos_acct_by_github_acct } from '../requests/indexer';
+import { DAPP_ADDRESS, MODULE_NAME, MODULE_URL } from '../config/constants'
+import { useWallet } from '@manahippo/aptos-wallet-adapter'
+import { useState } from 'react'
+import React from 'react'
+import { get_aptos_acct_by_github_acct } from '../requests/indexer'
 
-import { useSessionStorage } from 'react-use';
+import { useSessionStorage } from 'react-use'
 export default function Home() {
-  const { account, signAndSubmitTransaction } = useWallet();
-  const [githubIds, setGithubIds] = useState<Array<string>>(['']);
-  const [airdropAddresses, setAirdropAddresses] = useState<Array<string>>([]);
-  const [addressSession, setAddressSession] = useSessionStorage('airdropAddresses');
+  const { account, signAndSubmitTransaction } = useWallet()
+  const [githubIds, setGithubIds] = useState<Array<string>>([''])
+  const [showTable, setShowTable] = useState<{ github: string; address: string }[]>([])
+  const [addressSession, setAddressSession] = useSessionStorage('airdropAddresses')
   // input an array of github ids, return an array of aptos addresses
   const getAirdropAddresses = async () => {
     if (githubIds !== undefined) {
-      const out = await get_aptos_acct_by_github_acct(githubIds);
-      
-      let data: Array<string> = [];
-      console.log(out.data.result);
-      data = out.data.result;
-      
-      setAirdropAddresses(data);
-      console.log(airdropAddresses);
-      // let data: Array<string> = [];
-      // let okData: Array<string> = [];
-      // for (let index = 0; index < githubIds.length; index++) {
-      //   const githubId = githubIds[index];
-      //   const out = await get_aptos_acct_by_github_acct(githubId);
-      //   data.push(out.data.result.payload);
-      //   setAirdropAddresses([...data]);
-      //   if (out.data.result.status === 'ok') {
-      //     okData.push(out.data.result.payload);
-      //   }
-      // }
-      // setAddressSession(JSON.stringify(airdropAddresses));
+      const out = await get_aptos_acct_by_github_acct(githubIds)
+      const table = []
+      const session = []
+      if (out.data.result.length !== 0) {
+        for (let i = 0; i < out.data.result.length; i++) {
+          if (out.data.result[i] !== null) {
+            table.push({
+              github: githubIds[i],
+              address: out.data.result[i],
+            })
+            session.push(out.data.result[i])
+          }
+        }
+      }
+      setShowTable([...table])
+      setAddressSession(JSON.stringify(session))
     }
-  };
+  }
 
   const parseGithubIds = (rawInput: string): Array<string> => {
-    const splitInput = rawInput.split(',');
-    const parsedInput = splitInput.map((id) => id.trim());
-    return parsedInput;
-  };
-
-  console.log(githubIds);
+    const splitInput = rawInput.split(',')
+    const parsedInput = splitInput.map((id) => id.trim())
+    return parsedInput
+  }
 
   return (
     <div className=" p-4 w-[60%] m-auto flex flex-wrap shadow-2xl opacity-80 mb-10 justify-center ">
@@ -65,7 +59,7 @@ export default function Home() {
           Get Airdrop Addresses
         </button>
       </div>
-      {airdropAddresses.length !== 0 && (
+      {showTable.length !== 0 && (
         <div className="overflow-x-auto w-full">
           <h3 className="text-center font-bold my-5">Address By Github ID</h3>
           <table className="table table-compact w-full my-2">
@@ -77,21 +71,21 @@ export default function Home() {
               </tr>
             </thead>
             <tbody>
-              {airdropAddresses.map((address, index) => {
+              {showTable.map((table, index) => {
                 // TODO: click btn to make result fill to the Addresses@Coin airdropper
                 // both average and unaverage.
                 return (
                   <tr key={index}>
                     <th>{index + 1}</th>
-                    <td>{githubIds[index]}</td>
-                    <td>{address}</td>
+                    <td>{table.github}</td>
+                    <td>{table.address}</td>
                   </tr>
-                );
+                )
               })}
             </tbody>
           </table>
         </div>
       )}
     </div>
-  );
+  )
 }
